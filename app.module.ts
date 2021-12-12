0df70ddb66583e25724b35034cb69173b8a47d8d
Stephanie <51388595+Smichelle38@users.noreply.github.com>
2021-12-12 17:01:44 -0600

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SocialSharing } from '@ionic-native/social-sharing';
+import { ProviderService } from './provider.service';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, ProviderService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ dialog-provider.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ProviderService } from './provider.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogProviderService {
+
+  constructor() {}
+}
